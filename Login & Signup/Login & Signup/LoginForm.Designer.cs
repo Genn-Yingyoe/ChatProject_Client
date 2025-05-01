58885f05d90cf1114dbedf593ae560a50f3cd6b0
Kim TaeSu <zahara3@naver.com>
2025-05-01 11:32:15 +0900

    0.0.0
    Initial commit
    로그인 및 회원가입 간단하게 구현

---
+++ Login & Signup/Login & Signup/LoginForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Login___Signup
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(25, 290);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(300, 50);
+            this.txtID.TabIndex = 0;
+            this.txtID.Text = "ID\r\n";
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(25, 345);
+            this.txtPW.Multiline = true;
+            this.txtPW.Name = "txtPW";
+            this.txtPW.Size = new System.Drawing.Size(300, 50);
+            this.txtPW.TabIndex = 1;
+            this.txtPW.Text = "PW";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(50, 400);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(90, 40);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Location = new System.Drawing.Point(210, 400);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(90, 40);
+            this.btnSignup.TabIndex = 3;
+            this.btnSignup.Text = "SignUp";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 453);
+            this.Controls.Add(this.btnSignup);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.txtID);
+            this.Name = "LoginForm";
+            this.Text = "ChatMoa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnSignup;
+    }
+}
+
---
+++ Login & Signup/Login & Signup/LoginForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login___Signup
+{
+    public partial class LoginForm : Form
+    {
+        private string version = "0.0.0";   // 버전 정보
+        //0.0 -> 기본 로그인 기능 및 회원가입 기능
+        //0.1 -> 메인 대화창 기능 추가
+        //0.2 -> 친구 추가 기능
+
+        private const string UserDBPath = "UserDB.txt";
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inputID = txtID.Text.Trim();
+            string inputPW = txtPW.Text.Trim();
+
+            if (!File.Exists(UserDBPath))
+            {
+                MessageBox.Show("UserDB.txt 파일이 없습니다.");
+                return;
+            }
+
+            var lines = File.ReadAllLines(UserDBPath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                if (parts.Length < 6) continue; // 데이터가 부족한 경우 무시
+
+                if (parts[0] == inputID && parts[1] == inputPW) // 여기에 메인 대화창을 띄울 코드를 넣을것
+                {
+                    // 로그인 성공 시 메인 대화창을 띄우는 코드
+                    // MainChatForm mainChatForm = new MainChatForm();
+                    // mainChatForm.Show();
+                    // this.Hide();
+                    // 임시로 MessageBox로 대체
+                    {
+                        MessageBox.Show("로그인 성공!");
+                    return;
+                }
+            }
+
+            MessageBox.Show("로그인 실패. 아이디 또는 비밀번호를 확인하세요.");
+        }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            SignupForm signupForm = new SignupForm();
+            signupForm.ShowDialog();
+        }
+    }
+}
+
