5eafbe5ae99d90161aedbd0efd5cced966203b89
Kim TaeSu <71973011+Genn-Yingyoe@users.noreply.github.com>
2025-06-21 22:08:22 +0900

    update

--- ChatPlusMainSystem/MainSystem/LostPW.Designer.cs
+++ ChatPlusMainSystem/MainSystem/LostPW.Designer.cs
@@ -28,19 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLP = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtPW = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Question";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Answer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NewPW";
+            // 
+            // btnLP
+            // 
+            this.btnLP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(47)))), ((int)(((byte)(102)))));
+            this.btnLP.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLP.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(202)))), ((int)(((byte)(224)))));
+            this.btnLP.Font = new System.Drawing.Font("돋움", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLP.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLP.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLP.Location = new System.Drawing.Point(189, 155);
+            this.btnLP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLP.Name = "btnLP";
+            this.btnLP.Size = new System.Drawing.Size(79, 32);
+            this.btnLP.TabIndex = 11;
+            this.btnLP.Text = "Cancel";
+            this.btnLP.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(47)))), ((int)(((byte)(102)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(202)))), ((int)(((byte)(224)))));
+            this.button1.Font = new System.Drawing.Font("돋움", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(36, 155);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 32);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(78, 28);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(203, 21);
+            this.txtID.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 21);
+            this.textBox2.TabIndex = 14;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(78, 84);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(203, 21);
+            this.txtA.TabIndex = 15;
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(78, 111);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.Size = new System.Drawing.Size(203, 21);
+            this.txtPW.TabIndex = 16;
             // 
             // LostPW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(302, 198);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "LostPW";
             this.Text = "LostPW";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtPW;
     }
 }
